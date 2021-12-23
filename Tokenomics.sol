48d12134282338ab9a7dee8a35ac8c4ea3e5cbbb
UsmanFazil <usmanework99@gmail.com>
2021-12-23 23:06:05 +0500

    update Address function added

--- Tokenomics.sol
+++ Tokenomics.sol
@@ -5,38 +5,39 @@ pragma solidity ^0.8.0;
 //import "@openzeppelin/contracts/utils/Strings.sol";
 import "./SafeMath.sol";
 import "./IERC20.sol";
+import "./Ownable.sol";
 
-contract FloyxTokenomics {
+contract FloyxTokenomics is Ownable{
 
     using SafeMath for uint256;
+
     uint256 public constant unixtimeOneMonth = 60;//2592000; //60*60*24*30
     uint256 public deployedTime;
 
+    IERC20 internal floyx;
+
     mapping(string => address)public roles;
     mapping(address => uint256)public tokenAllowance;
-
     mapping(address => uint256) public paymentPerMonth;
     mapping(address => uint) public remainingInstallments;
     mapping(address => uint) public completedInstallments;
 
-    IERC20 internal floyx;
-
     event FundsReleased(address indexed recepient, uint256 amount);
-                    // 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2,10,10,5,5,2
 
     constructor(string[] memory roles_, address[] memory addresses_, uint256[] memory allowancePercentage_, 
         uint256[] memory installmentPercentage_, address floyxAddress){
 
         require(addresses_.length == roles_.length);
+
         deployedTime = block.timestamp;
         floyx = IERC20(floyxAddress);
-
         uint256 totalSupply = floyx.totalSupply();
-        for (uint8 i=0; i< addresses_.length; i++){
-            roles[roles_[i]] = addresses_[i];
 
-            tokenAllowance[addresses_[i]] = percentage(totalSupply, allowancePercentage_[i]) ; // total supply percentage
-            paymentPerMonth[addresses_[i]] = percentage(tokenAllowance[addresses_[i]], installmentPercentage_[i]); // installments of supply
+        for (uint8 i=0; i< addresses_.length; i++){
+
+            roles[roles_[i]] = addresses_[i];
+            tokenAllowance[addresses_[i]] = _percentage(totalSupply, allowancePercentage_[i]) ; // total supply percentage
+            paymentPerMonth[addresses_[i]] = _percentage(tokenAllowance[addresses_[i]], installmentPercentage_[i]); // installments of supply
             remainingInstallments[addresses_[i]] =  tokenAllowance[addresses_[i]].div(paymentPerMonth[addresses_[i]]);
         }
     }
@@ -44,6 +45,7 @@ contract FloyxTokenomics {
     function distributeInstallment(address recepient, uint256 monthsToPay) public{
         uint256 amountDue=0;
         monthsToPay = monthsToPay.sub(completedInstallments[recepient]);
+
         for(uint256 len=monthsToPay; len>0; len--){
             if(remainingInstallments[recepient] == 0){
                 break;
@@ -55,23 +57,22 @@ contract FloyxTokenomics {
         }
 
         if (amountDue > 0) {
-            floxyTransfer(amountDue,recepient);
+            _floxyTransfer(amountDue,recepient);
         }
     }
 
     function teamClaim()public{
         string memory role = "team";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(0);
+        uint256 monthsToPay = _elapsedMonths(0);
         require (monthsToPay > 0, "No due Payments yet");
-
         distributeInstallment(msg.sender,monthsToPay);
     }
 
     function advisorClaim()public{
         string memory role = "advisorsAndPartnership";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(0);
+        uint256 monthsToPay = _elapsedMonths(0);
         monthsToPay = monthsToPay.div(3);
         require (monthsToPay > 0, "No due Payments yet");
 
@@ -81,7 +82,7 @@ contract FloyxTokenomics {
     function marketingClaim()public{
         string memory role = "marketing";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(0);
+        uint256 monthsToPay = _elapsedMonths(0);
         require (monthsToPay > 0, "No due Payments yet");
 
         distributeInstallment(msg.sender,monthsToPay);
@@ -97,7 +98,7 @@ contract FloyxTokenomics {
     function developmentClaim()public{
         string memory role = "development";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(0);
+        uint256 monthsToPay = _elapsedMonths(0);
         require (monthsToPay > 0, "No due Payments yet");
 
         distributeInstallment(msg.sender,monthsToPay);
@@ -106,7 +107,7 @@ contract FloyxTokenomics {
     function ecosystemClaim()public{
         string memory role = "ecosystemFunds";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(0);
+        uint256 monthsToPay = _elapsedMonths(0);
         require (monthsToPay > 0, "No due Payments yet");
 
         distributeInstallment(msg.sender,monthsToPay);
@@ -123,7 +124,7 @@ contract FloyxTokenomics {
     function airDropClaim()public{
         string memory role = "ecosystemFunds";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(0);
+        uint256 monthsToPay = _elapsedMonths(0);
         require (monthsToPay > 0, "No due Payments yet");
 
         distributeInstallment(msg.sender,monthsToPay);
@@ -132,7 +133,7 @@ contract FloyxTokenomics {
     function grantsClaim()public{
         string memory role = "ecosystemFunds";
         _verifyClaim(role, msg.sender);
-        uint256 monthsToPay = elapsedMonths(3); // 3 months lockout period
+        uint256 monthsToPay = _elapsedMonths(3); // 3 months lockout period
         require (monthsToPay > 0, "No due Payments yet");
 
         distributeInstallment(msg.sender,monthsToPay);
@@ -144,17 +145,26 @@ contract FloyxTokenomics {
         require(remainingInstallments[user_] > 0, "installments completed");
     }
 
-    function floxyTransfer(uint256 amountDue,address recepient) internal{
+    function updateAddress(string[] memory roles_, address[] memory addresses_)public onlyOwner{
+        require(addresses_.length == roles_.length);
+        
+        for(uint8 i = 0; i< roles_.length; i++){
+            require(roles[roles_[i]] != address(0), "role does not exists");
+            roles[roles_[i]] = addresses_[i];
+        }
+    }
+
+    function _floxyTransfer(uint256 amountDue,address recepient) internal{
             tokenAllowance[recepient] = tokenAllowance[recepient].sub(amountDue);
             floyx.transfer(recepient,amountDue);
             emit FundsReleased(recepient, amountDue);
     }
   
-    function percentage(uint256 totalAmount_,uint256 _percentage) internal pure returns(uint256) {
-        return (totalAmount_.mul(_percentage).div(100));
+    function _percentage(uint256 totalAmount_,uint256 percentage_) internal pure returns(uint256) {
+        return (totalAmount_.mul(percentage_).div(100));
     }
 
-    function elapsedMonths(uint256 _lockPeriod) internal view returns(uint256) {
+    function _elapsedMonths(uint256 _lockPeriod) internal view returns(uint256) {
         uint256 presentTime = block.timestamp;
         uint256 elapsedTime = presentTime.sub(deployedTime);
      
